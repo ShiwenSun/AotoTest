c56ad22ce083bfd77d43a3045d7a191375dfb888
ShiwenSun <wyf6971sym0322>
2018-06-15 00:46:41 +0800

    dataprovider文件修改

--- shiwen/src/main/java/com/course/testng/paramter/DataProviderTest.java
+++ shiwen/src/main/java/com/course/testng/paramter/DataProviderTest.java
@@ -1,7 +1,10 @@
 package com.course.testng.paramter;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
 
 public class DataProviderTest {
 
@@ -19,4 +22,33 @@ public class DataProviderTest {
         };
         return o;
     }
+
+
+
+    @Test(dataProvider = "datemethod")
+    public  void  test1(String name,int age){
+        System.out.println("name:" + name + ";age:" + age);
+    }
+    @Test(dataProvider = "datemethod")
+    public  void  test2(String name,int age){
+        System.out.println("name:" + name + ";age:" + age);
+    }
+    @DataProvider(name = "datemethod")
+    public Object[][] DataMethod(Method method){
+        Object[][] objects= null;
+
+        if (method.getName().equals("test1")){
+            objects = new  Object[][]{
+                    {"aaaa",10},
+                    {"bbbb",20}
+            };
+        }else if (method.getName().equals("test2")){
+            objects = new  Object[][]{
+                    {"CCCC",30},
+                    {"DDDD",40}
+            };
+        }
+        return objects;
+    }
+
 }
